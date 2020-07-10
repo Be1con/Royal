c383c93be642d0ebdf028e8ef9dd3dc330f8657b
Kittanan Jamnongrit <be1con-tr@live.com>
2020-07-10 14:45:58 +0700

    Initial Project Starter

---
+++ src/Components/Menubar/Menubar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Navbar } from 'materialize-css';
+import './Menubar.css';
+
+class Menubar extends Component {
+    render(){
+        return(
+            <Navbar>
+                
+            </Navbar>
+        )
+    }
+}
+
+export default Menubar
---
+++ src/Pages/Pages.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Pages.css';
+
+
+class Pages extends Component {
+    render() {
+        return(
+            <div>
+                <Menubar />
+            </div>
+        );
+    }
+}
+
+export default Pages;
