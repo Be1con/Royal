129e8bdb7342a0ad6370e292bc808f0fa423f1e4
Kittanan Jamnongrit <be1con-tr@live.com>
2020-07-10 15:12:09 +0700

    Fix Menubar bug

--- src/Components/Menubar/Menubar.js
+++ src/Components/Menubar/Menubar.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { Navbar } from 'materialize-css';
 import './Menubar.css';
 
 class Menubar extends Component {
     render(){
         return(
-            <Navbar>
+            <div>
                 
-            </Navbar>
+            </div>
         )
     }
 }
