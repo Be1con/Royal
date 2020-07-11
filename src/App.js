0c0b4c175422b698d873e5efbdefe923ba4414dc
zuckpa1999 <zuckpa1999@gmail.com>
2020-07-11 23:09:15 +0700

    fetch data from firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,70 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Pages from './Pages/Pages';
+import * as firebase from 'firebase/app';
 
 class App extends Component {
+    
+    constructor(){
+    super()
+    this.state = {
+
+       FirstName : 'fefe',
+       obj : {},
+       s : 'c',
+       
+
+    }
+    }
+        
+    componentDidMount() {
+
+            const rootRef = firebase.database().ref('Mastersheet')
+            // console.log(rootRef)
+         
+            const nameRef = rootRef.child('FirstName')
+            // console.log(nameRef)
+            rootRef.on('value', (snap) => {
+                console.log(snap.val())
+                // alert(snap.val())
+              
+                this.setState({
+
+                obj : snap.val(),
+                s : 'z',
+                FirstName : 'zzzzzz'
+            
+    
+            })
+            console.log((this.state.obj))
+            console.log(this.state.s)
+            // const a = Object.entries(this.state.obj)
+            // console.log(a)
+            // console.log(obj)
+            // console.log(snap); 
+            // console.log(this.state.FirstName)    
+            console.log((this.state.obj[2].Rank))
+            console.log(this.state.s)
+            console.log(this.state.FirstName)
+            });
+           
+        console.log(this.state.obj)
+        console.log(this.state.s)
+        console.log(this.state.FirstName)
+    }
+
+
     render(){
         return (
             <BrowserRouter>
                 <div>
                     <Pages />
+                    {Object.keys(this.state.obj).map( (item,i) => <div key={i}>{item}</div>)}
+                      {Object.keys(this.state.obj).map( (item,i) => <div key={i}>{item}</div>)
+}
+                     {Object.entries(this.state.obj).map(([key, value]) => console.log(`${value.LastName}`))}  
+{Object.entries(this.state.obj).map(([key, value]) => <h1 key={value}>{value.FirstName}</h1>)}
+                     <h1>fenuifneufnui</h1>
                 </div>
             </BrowserRouter>
         );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase'; 
+
+
+var  config  = {
+  apiKey : "AIzaSyBo8X8wy49depPwn_K8RcXYPPDERFddUqg",
+  authDomain: "test-27239.firebaseapp.com",
+  databaseURL: "https://test-27239.firebaseio.com",
+  projectId: "test-27239",
+  storageBucket: "test-27239.appspot.com",
+  messagingSenderId: "556808780992",
+  appId: "1:556808780992:web:67781fd6c9e380b0498125",
+  measurementId: "G-QDL4JMY4C2"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <React.StrictMode>
