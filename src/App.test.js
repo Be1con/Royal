eda31cc33dcce65b71eb6aa662f4615d3018cae4
Kittanan Jamnongrit <be1con-tr@live.com>
2020-07-12 16:51:53 +0700

    Remove test case

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('render test', () => {
-  const { getByText } = render(<App />);
-  const allElement = getByText(/test/i);
-  expect(allElement).toBeInTheDocument();
-});
