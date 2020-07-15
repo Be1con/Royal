da21729eecfe06a49e7972289063ee80697dc7ae
Kittanan Jamnongrit <be1con-tr@live.com>
2020-07-15 16:29:33 +0700

    Remove Materialize (again)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import 'materialize-css/dist/css/materialize.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import Pages from './Pages/Pages';
 
